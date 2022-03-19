9ea45f475270c3e219c74095a60f95a7e339a707
gc01873 <42818728+gc01873@users.noreply.github.com>
2022-03-19 19:09:43 -0400

    Update SQL_ASSIGNMENT_3.sql

--- SQL_ASSIGNMENT_3.sql
+++ SQL_ASSIGNMENT_3.sql
@@ -50,26 +50,98 @@ Order BY pProductID
 --4. List all Customer Cities and total products ordered by that city.
 --Select field1, Aggregate(field2) Over([Partition By field3] Order By field4) From Table.
 
-/*Select  CustomerID, COUNT(OrderID)
-From Orders
-Group By CustomerID*/
+/*
+select sub.city AS [City], sub.[Product Quantity Sold] AS [Product Quantity Sold] , row_number() Over (partition by sub.city Order by sub.city) AS num INTO #tempCusCity
+FROM
+(select  c.city,  sum( sub.Quanity_sum) OVER(partition by c.city order by c.city) AS [Product Quantity Sold]
+from customers c join 
+(select  o.customerID, sum(od.quantity) OVER(partition by od.OrderID Order by od.productID ) AS Quanity_sum, od.OrderID, ProductID
+From Orders o  join [Order Details Extended] Od 
+on o.orderId = od.OrderID) sub on c.CustomerID=sub.customerID) sub
 
 
+WITH cte AS (
+    SELECT 
+        City, [Product Quantity Sold],
+        ROW_NUMBER() OVER (
+           partition by city Order by city
+        ) row_num
+     FROM 
+       #tempCusCity
+)
+DELETE FROM cte
+WHERE row_num > 1;
+
+Select City,[Product Quantity Sold]  FRom #tempCusCity
+order by city
+
+*/
 
 --5 
 
---6
+--6 List all Customer Cities that have ordered at least two different kinds of products.
+
+--TRY THIS QUERY AGAIN
+
+/*
+Select Distinct c.city AS [Cities with more than 1 Product ordered]
+From customers c JOIN (select  o.customerID, COUNT(DISTINCT od.ProductID) AS [Product Count]
+From Orders o join [Order Details Extended] Od 
+on o.orderId = od.OrderID
+Group by o.CustomerID
+Having COUNT( DISTINCT od.ProductID) >= 2
+) sub on 
+c.CustomerID = sub.customerID
+*/
+
+--7 List all Customers who have ordered products, but have the ‘ship city’ on the order different from their own customer cities.
 
 
---7
 
---8
+/*
+SELECT Distinct c.City
+From customers c JOin (
+select  o.customerID, o.ShipCity
+From Orders o right join [Order Details Extended] Od 
+on o.orderId = od.OrderID) sub ON
+c.customerID = sub.customerID AND c.city <> sub.ShipCity
+*/
 
---9
+--8   List 5 most popular products, their average price, and the customer city that ordered most quantity of it.
+/* --DROP TABLE #TFIVE
+ SELECT * INTO #TFive FROM ( 
+Select TOP 5 productID AS prodID, AVG(UnitPrice) AS Average_UnitPrice , COUNT(productID) AS [Products Sold],  ROW_NUMBER () OVER (Order by COUNT(productID) DESC ) AS RANK_SOLD
+FRom [Order Details Extended]
+group by productID 
+)  as x
+--LOOK OVER REVISIT
+--Select  c.customerID,fsub.customerID, fsub.ProductID, fsub.[products sold],fsub.OrderID, fsub.AVerage_UnitPrice
+SELECT  z.RANK_SOLD, MAX(z.Product_Count), z.Average_unitPRice 
+FROM (
+    select c.city, c.customerID, count(fsub.prodID) OVER(partition by c.city ORDER By fsub.RANK_SOLD) AS Product_Count, fsub.[products sold],fsub.orderID, fsub.RANK_SOLD, fsub.AVerage_UnitPrice
+FROM (
+    Select o.customerID, od.orderID, od.productID, f.prodID, f.[products sold],f.AVerage_UnitPrice, f.Rank_Sold
+
+FROM #TFive f  join  [Order Details Extended] od on 
+od.productID = f.prodID join orders o ON
+od.OrderID = o.orderID) fsub join customers c ON
+fsub.CustomerID = c.CustomerID
+) z GROUP BY z.RANK_SOLD,z.Average_unitPrice
+*/
+
+
+
+
+
+--9     List all cities that have never ordered something but we have employees there.
+
+--a.      Use sub-query
+
+--b.      Do not use sub-query
 
 --10
 
---11 You can use a common table expression such as this example to delet duplicate rows in a table
+--11 You can use a common table expression such as this example to delete duplicate rows in a table
 
 /*WITH cte AS (
     SELECT 
@@ -92,7 +164,6 @@ Group By CustomerID*/
 )
 DELETE FROM cte
 WHERE row_num > 1;*/
-
 
 
 
